6d6cf27506859ceaeac2391a912981fcd9d5bc05
Sharelison Caldera <sharelison@hotmail.com>
2018-12-13 20:16:02 +0100

    should analyze every object in array

--- src/test/resources/integrationtest/expected/Children.java
+++ src/test/resources/integrationtest/expected/Children.java
@@ -8,6 +8,8 @@ public class Children {
     private String lastName;
     @JsonProperty("name")
     private String name;
+    @JsonProperty("job")
+    private String job;
 
     public String getLastName(){
         return this.lastName;
@@ -23,6 +25,14 @@ public class Children {
 
     public void setName(String name){
         this.name = name;
+    }
+
+    public String getJob(){
+        return this.job;
+    }
+
+    public void setJob(String job){
+        this.job = job;
     }
 
 }
